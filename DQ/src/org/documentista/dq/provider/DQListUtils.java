bd1f67284cecb053d259be4800afddf831868a5a
Tom Dolan <tgdolan@gmail.com>
2012-04-22 15:47:20 -0400

    Updating for current db creds and refactoring.

--- DQ/src/org/documentista/dq/provider/DQListUtils.java
+++ DQ/src/org/documentista/dq/provider/DQListUtils.java
@@ -1,10 +1,11 @@
 package org.documentista.dq.provider;
 
-import java.util.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+
+import org.documentista.utils.data.USStates;
 
 
 
@@ -16,27 +17,37 @@ public class DQListUtils {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		Connection conn = null;
+
+       
 		DQList dqList = new DQList();
-		List<DQ> dairyQueens = dqList.getDairyQueens(DQ_URL);
-		for (DQ dq : dairyQueens) {
-			String name = dq.getName();
-			String street = dq.getStreetAddr();
-			String city = dq.getCity();
-			String state = dq.getState();
-			String zip = dq.getZip();
-			String webSite = dq.getUrl();
-			String telno = dq.getTelno();
-			Double longitude = 0.0;
-			Double latitude = 0.0;
-			if(dq.getLongitude() != null && !dq.getLongitude().equals("")) {
-				longitude = Double.valueOf(dq.getLongitude());
+		
+		for (USStates us_state : USStates.values()) {
+			String dqURL = "http://www.dairyqueen.com/us-en/stores/" + us_state + "/";
+			List<DQ> dairyQueens = dqList.getDairyQueens(DQ_URL);
+			for (DQ dq : dairyQueens) {
+				String name = dq.getName();
+				String street = dq.getStreetAddr();
+				String city = dq.getCity();
+				String state = dq.getState();
+				String zip = dq.getZip();
+				String webSite = dq.getUrl();
+				String telno = dq.getTelno();
+				Double longitude = 0.0;
+				Double latitude = 0.0;
+				if(dq.getLongitude() != null && !dq.getLongitude().equals("")) {
+					longitude = Double.valueOf(dq.getLongitude());
+				}
+				if (dq.getLatitude() != null && !dq.getLatitude().equals("")) {
+					latitude = Double.valueOf(dq.getLatitude());
+				}
+				
+				insertDQ(name, street, city, state, zip, webSite, telno, longitude, latitude);
 			}
-			if (dq.getLatitude() != null && !dq.getLatitude().equals("")) {
-				latitude = Double.valueOf(dq.getLatitude());
-			}
-			
-			insertDQ(name, street, city, state, zip, webSite, telno, longitude, latitude);
 		}
+		
+		
 	}
 
 	public static void insertDQ(String name, String street, String city,
@@ -48,9 +59,12 @@ public class DQListUtils {
 
         try
         {
-            String userName = "tom";
+            String userName = "root";
             String password = "hon360da";
-            String url = "jdbc:mysql://localhost:3306/DQ";
+//        	String userName = "tgdolanc_tom";
+//            String password = "hondacl360";
+//            String url = "jdbc:mysql://tgdolan.com:3306/tgdolanc_diners";
+           String url = "jdbc:mysql://localhost:3306/DQ";
             Class.forName ("com.mysql.jdbc.Driver").newInstance ();
             conn = DriverManager.getConnection (url, userName, password);
             Statement stmt = conn.createStatement();
